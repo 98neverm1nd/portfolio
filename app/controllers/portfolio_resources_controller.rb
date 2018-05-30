238d8693e24c1f81ab655481e780a7fba33561ed
98neverm1nd <98neverm1nd@gmail.com>
2018-05-30 23:31:10 +0300

    created 2 methods: new and create for portfolio model

--- app/controllers/portfolio_resources_controller.rb
+++ app/controllers/portfolio_resources_controller.rb
@@ -2,4 +2,24 @@ class PortfolioResourcesController < ApplicationController
 	def index
 		@portfolio_items = PortfolioResource.all
 	end
+
+	def new
+		@portfolio_item = PortfolioResource.new
+	end
+	def create
+  	@portfolio_item = PortfolioResource.new(params.require(:portfolio_resource).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to @portfolio_item, notice: 'Portfolio item was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio_item }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+def show
+  end
+
 end
