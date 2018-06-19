c3aa532bd9c1f8bc20bcd72e384982720f5060db
98neverm1nd <98neverm1nd@gmail.com>
2018-06-20 00:44:28 +0300

    add custom toggle action for status

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolio_resources#show', as: 'portfolio_resources_show'
   get 'about', to: 'pages#about'
   get 'contacts', to: 'pages#contact'
-  resources :blogs
+  resources :blogs do
+  	member do
+  		get :toggle_status
+  	end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
