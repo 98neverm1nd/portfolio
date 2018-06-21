27c82dd9076e34d3227c49bb2e052b51d51c1eda
98neverm1nd <98neverm1nd@gmail.com>
2018-06-22 00:49:45 +0300

    added concern Placeholder

--- app/controllers/portfolio_resources_controller.rb
+++ app/controllers/portfolio_resources_controller.rb
@@ -15,7 +15,7 @@ class PortfolioResourcesController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to @portfolio_item, notice: 'Portfolio item was successfully created.' }
+        format.html { redirect_to portfolio_resources_url, notice: 'Portfolio item was successfully created.' }
         format.json { render :show, status: :created, location: @portfolio_item }
       else
         format.html { render :new }
--- app/models/portfolio_resource.rb
+++ app/models/portfolio_resource.rb
@@ -2,10 +2,18 @@ class PortfolioResource < ApplicationRecord
 	def index
 		@portfolio_items = PortfolioResource.all
 	end
-
+	include Placeholder
 	validates_presence_of :title, :body, :main_image, :thumb_image
 
 	def self.angular
 		where(subtitle: 'Angular')
+	end
+
+	scope :ror, -> {where(subtitle: 'Ruby on Rails')}
+
+	after_initialize :set_defaults
+	def set_defaults
+		self.main_image ||= Placeholder.image_generator(height: 700, width: 400)
+		self.thumb_image ||= Placeholder.image_generator(height: 350, width: 150)
 	end
 end
