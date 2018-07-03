6fea0bf7a38dcbe5fc0565ef3d3c7ccb0446043c
98neverm1nd <98neverm1nd@gmail.com>
2018-07-03 21:13:59 +0300

    visual update for technologies

--- app/controllers/portfolio_resources_controller.rb
+++ app/controllers/portfolio_resources_controller.rb
@@ -9,9 +9,10 @@ class PortfolioResourcesController < ApplicationController
 
 	def new
 		@portfolio_item = PortfolioResource.new
+    3.times { @portfolio_item.technologies.build }
 	end
 	def create
-  	@portfolio_item = PortfolioResource.new(params.require(:portfolio_resource).permit(:title, :subtitle, :body))
+  	@portfolio_item = PortfolioResource.new(params.require(:portfolio_resource).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
